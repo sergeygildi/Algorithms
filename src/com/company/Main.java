ca79ac96e7720464495274fd92f0345dee0782f5
”Serhii <sergeygildi@gmail.com>
2022-08-30 12:01:02 +0300

    Revert "Cleaned directory"
    This reverts commit 8b4096e9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hello Wourld");// write your code here
     }
 }
+=======
+	// write your code here
+    }
+}
+>>>>>>> parent of 8b4096e (Cleaned directory)
