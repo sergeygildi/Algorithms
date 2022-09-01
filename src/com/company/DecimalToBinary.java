8532dad8bb47f9412b6a11d007dc71e2acd6627f
”Serhii <sergeygildi@gmail.com>
2022-09-01 13:42:02 +0300

    Made changes into the code, including edits. Also implemented work with negative numbers

--- src/com/company/DecimalToBinary.java
+++ src/com/company/DecimalToBinary.java
@@ -1,36 +1,41 @@
 package com.company;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+/**
+ * An implementation of the decimal to binary conversion algorithm
+ */
 public class DecimalToBinary {
 
-    private static final int DECIMAL_NUMBER = 122;
-    private static Logger logger = Logger.getGlobal();
+    private static final int DECIMAL_NUMBER = -128;
 
-    private static StringBuilder decimalToBinary(int decimalNumber) {
-        StringBuilder sb = new StringBuilder();
+    private static String decimalToBinary(int decimalNumber) {
 
-        int tempDecimalNumber = decimalNumber;
+        int binaryRepresentationOfANumber;
 
-        boolean isDone = false;
-        while (!isDone) {
-            if (tempDecimalNumber%2==0) {
-                tempDecimalNumber = tempDecimalNumber / 2;
-                sb.append("0");
-            } else {
-                tempDecimalNumber = (tempDecimalNumber - 1) / 2;
-                sb.append("1");
-            }
-            if (tempDecimalNumber == 0) {
-                isDone = true;
-            }
+        if (DECIMAL_NUMBER < 0) {
+            binaryRepresentationOfANumber = getBinaryRepresentationOfANumber(Math.abs(decimalNumber));
+            return String.valueOf(~binaryRepresentationOfANumber + 1);
+        } else {
+            binaryRepresentationOfANumber = getBinaryRepresentationOfANumber(decimalNumber);
+            return String.valueOf(binaryRepresentationOfANumber);
         }
-        return sb.reverse();
     }
 
-    private static void printResult(StringBuilder stringBuilder) {
-        logger.log(Level.INFO, String.valueOf(stringBuilder));
+    private static int getBinaryRepresentationOfANumber(int tempDecimalNumber) {
+        StringBuilder sb = new StringBuilder();
+        do {
+            if (tempDecimalNumber % 2 == 0) {
+                tempDecimalNumber = tempDecimalNumber >> 1;
+                sb.append("0");
+            } else {
+                tempDecimalNumber = (tempDecimalNumber - 1) >> 1;
+                sb.append("1");
+            }
+        } while (tempDecimalNumber != 0);
+        return Integer.parseInt(sb.reverse().toString());
+    }
+
+    private static void printResult(String result) {
+        System.out.print(result);
     }
 
     public static void main(String[] args) {
