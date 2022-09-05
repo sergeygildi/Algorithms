d2300e9b6fe56a033f40317aee1db0a1797740e7
Serhii Gildi <14317559+sergeygildi@users.noreply.github.com>
2022-09-05 20:27:46 +0300

    Fix: Code refactored, some processes optimized

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -1,39 +1,35 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class BinarySearch {
-
-    private static final Logger LOG = Logger.getGlobal();
-    public static final int[] ARRAY = {0,1,2,3,4,5,6,7,8,9,10};
+    public static final int[] NUMBERS = {30, 11, 22, 43, 54, 65, 76, 87, 98, 9, -10};
 
     public static void main(String[] args) {
-        binarySearch1(ARRAY, 1);
+        binarySearch(NUMBERS, 3);
     }
 
-    private static void binarySearch1(int[] array, int key) {
-        Arrays.sort(array);
+    private static void binarySearch(int[] numbers, int elementToBeFound) {
+        Arrays.sort(numbers);
 
         int first = 0;
-        int last = array.length - 1;
-        int mid = (first + last) / 2;
+        int last = numbers.length - 1;
+        int pivot = (first + last) >> 1;
 
         while (first <= last) {
-            if (array[mid] == key) {
-                LOG.log (Level.INFO, key + " found at location " + mid);
+            if (numbers[pivot] == elementToBeFound) {
+                System.out.printf("%s found at location %s", elementToBeFound, pivot);
                 break;
-            } else if (array[mid] > key) {
-                last = mid - 1;
-            } else if (array[mid] < key) {
-                first = mid + 1;
+            } else if (numbers[pivot] > elementToBeFound) {
+                last = pivot--;
+            } else if (numbers[pivot] < elementToBeFound) {
+                first = pivot++;
             }
-            mid = (first + last) / 2;
+            pivot = (first + last) >> 1;
         }
 
         if (first > last) {
-            LOG.log (Level.INFO, key + " is not present in the list.\n");
+            System.out.printf("%s is not present in the list.\n", elementToBeFound);
         }
     }
 }
