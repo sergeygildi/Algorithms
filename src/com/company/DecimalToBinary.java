cdf8bb7578e39a9a0ebdc658a1a75e0c769d7501
sergeygildi <sergeygildi@gmail.com>
2022-09-09 21:42:18 +0300

    Fix: - The method is implemented more concisely: the code has become cleaner

--- src/com/company/DecimalToBinary.java
+++ src/com/company/DecimalToBinary.java
@@ -1,44 +1,22 @@
 package com.company;
 
 /**
- * An implementation of the decimal to binary conversion algorithm
+ * An implementation of the algorithm that converts a number from decimal to binary
  */
 public class DecimalToBinary {
 
-    private static final int DECIMAL_NUMBER = -128;
-
-    private static String decimalToBinary(int decimalNumber) {
-
-        int binaryRepresentationOfANumber;
-
-        if (DECIMAL_NUMBER < 0) {
-            binaryRepresentationOfANumber = getBinaryRepresentationOfANumber(Math.abs(decimalNumber));
-            return String.valueOf(~binaryRepresentationOfANumber + 1);
-        } else {
-            binaryRepresentationOfANumber = getBinaryRepresentationOfANumber(decimalNumber);
-            return String.valueOf(binaryRepresentationOfANumber);
-        }
-    }
-
-    private static int getBinaryRepresentationOfANumber(int tempDecimalNumber) {
+    private static void decimalToBinary(int decimalNumber) {
         StringBuilder sb = new StringBuilder();
-        do {
-            if (tempDecimalNumber % 2 == 0) {
-                tempDecimalNumber = tempDecimalNumber >> 1;
-                sb.append("0");
-            } else {
-                tempDecimalNumber = (tempDecimalNumber - 1) >> 1;
-                sb.append("1");
-            }
-        } while (tempDecimalNumber != 0);
-        return Integer.parseInt(sb.reverse().toString());
+        while (decimalNumber != 0) {
+            sb.append(decimalNumber < 0 ? '1' : '0');
+            decimalNumber <<= 1;
+        }
+        System.out.println(sb.reverse().toString());
     }
-
-    private static void printResult(String result) {
-        System.out.print(result);
-    }
-
     public static void main(String[] args) {
-        printResult(decimalToBinary(DECIMAL_NUMBER));
+        decimalToBinary(10);
+        decimalToBinary(-10);
+        decimalToBinary(128);
+        decimalToBinary(-128);
     }
 }
