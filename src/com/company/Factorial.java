1b26e63fd453c9c440c7d1756ff128ce758c4d89
sergeygildi <sergeygildi@gmail.com>
2022-09-19 17:30:54 +0300

    Fix: -Recommendations received

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -7,15 +7,10 @@ import java.math.BigInteger;
  */
 public class Factorial {
 
-    public static void main(String[] args) {
-        getFactorial("4");
-        getFactorial("-4");
-    }
-
     /**
      * The long datatype can be used for factorials only for n <= 20.
      * For larger values of n, we can use the BigInteger class from the java.Math package,
-     * which can hold values up to 2^Integer.MAX_VALUE;
+     * which can hold values up to {@code 2^Integer.MAX_VALUE}.
      *
      * @param targetNumberString
      * @return BigInteger
@@ -28,9 +23,14 @@ public class Factorial {
                 target = target.subtract(BigInteger.ONE);
                 factorial = factorial.multiply(target);
             }
-            System.out.println(factorial);
             return factorial;
         }
         return BigInteger.ONE;
     }
+
+    public static void main(String[] args) {
+        getFactorial("4");
+        getFactorial("-4");
+    }
+
 }
