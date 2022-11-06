2f32a3759e5320bf23c7ccb2dc1ee67a76ab542d
serhiihildi <sergeygildi@gmail.com>
2022-11-06 11:45:45 +0200

    feat: implement LinkedList with tests

---
+++ src/main/java/com/algorithms/collections/LinkedList.java
@@ -0,0 +1,68 @@
+package com.algorithms.collections;
+
+import java.util.NoSuchElementException;
+
+/**
+ * An implementation of the LinkedList Collection
+ */
+public class LinkedList<T> {
+    class Node<E> {
+        public E data;
+        public Node<E> next;
+        public Node<E> prev;
+
+        public Node(Node<E> prev, E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node<T> head;
+
+    private Node<T> tail;
+
+    private int size = 0;
+
+    public void addFirst(T element) {
+        final Node<T> oldHead = head;
+        final Node<T> newNode = new Node<>(null, element, oldHead);
+        head = newNode;
+        if (oldHead == null)
+            tail = newNode;
+        else
+            oldHead.prev = newNode;
+        size++;
+    }
+
+    public T getFirst() {
+        final Node<T> curHead = head;
+        if (curHead == null)
+            throw new NoSuchElementException();
+        return head.data;
+    }
+
+    public void addLast(T element) {
+        final Node<T> oldTail = tail;
+        final Node<T> newNode = new Node<>(oldTail, element, null);
+        tail = newNode;
+        if (oldTail == null)
+            head = newNode;
+        else
+            oldTail.next = oldTail;
+        size++;
+    }
+
+    public T getLast() {
+        final Node<T> curTail = tail;
+        if (curTail == null)
+            throw new NoSuchElementException();
+        return tail.data;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+}
+
---
+++ src/test/java/com/algorithms/collections/LinkedListTest.java
@@ -0,0 +1,43 @@
+package com.algorithms.collections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LinkedListTest {
+
+    private LinkedList<Integer> integers;
+
+    @BeforeEach
+    void setUp() {
+        integers = new LinkedList<>();
+        for (int i = 0; i < 2000; i++) {
+            integers.addLast(i);
+        }
+    }
+
+    @Test
+    @DisplayName("Should return correct size of the list")
+    void add() {
+        assertEquals(2000, integers.size());
+    }
+
+    @Test
+    @DisplayName("Should return correct first element")
+    void getFirst() {
+        Integer actual = this.integers.getFirst();
+
+        assertEquals(0, actual);
+    }
+
+    @Test
+    @DisplayName("Should return correct last element")
+    void getLast() {
+        Integer actual = this.integers.getLast();
+
+        assertEquals(1999, actual);
+    }
+
+}
